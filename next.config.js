e6a742394ba66ba88bdc9a90fc5f78ea04d29250
GiorgyArmani <giorgymodoginseng@gmail.com>
2025-06-25 01:42:38 -0300

    changes on next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ['example.com'],
+    domains: ['i.imgur.com'],
   },
 };
 
 module.exports = nextConfig;
+
